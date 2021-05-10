b2e560ae1ed1ffd7060085f95f0b5305b8fa30f5
seanmajorpayne <seanmajorpayne@gmail.com>
2021-05-10 20:48:15 +0900

    adding interfaces

--- nft/scripts/helpful_scripts.py
+++ nft/scripts/helpful_scripts.py
@@ -5,4 +5,4 @@ def fund_advanced_collectible(nft_contract):
     link_token = interface.LinkTokenInterface(
         config['networks'][network.show_active()]['link_token']
     )
-    link_token.transfer(nft_contract, 10000000000000000, {"from": dev})
+    link_token.transfer(nft_contract, 100000000000000000, {"from": dev})
