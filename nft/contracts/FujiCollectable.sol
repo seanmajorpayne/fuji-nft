a83758f3d78c649380ddc4f6b3dedb1c4a7e2cdc
seanmajorpayne <seanmajorpayne@gmail.com>
2021-05-10 15:57:56 +0900

    fixing import bug

--- nft/contracts/FujiCollectable.sol
+++ nft/contracts/FujiCollectable.sol
@@ -1,4 +1,8 @@
 pragma solidity 0.6.6;
 
-import "@openzepplin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
+
+contract FujiCollectable is ERC721, VRFConsumerBase {
+
+}
