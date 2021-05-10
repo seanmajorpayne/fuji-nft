acf660897290b7f63228079114c23744c6d53443
seanmajorpayne <seanmajorpayne@gmail.com>
2021-05-10 19:22:29 +0900

    add funding script

--- nft/scripts/fuji_collectible/fund_collectible.py
+++ nft/scripts/fuji_collectible/fund_collectible.py
@@ -1,2 +1,6 @@
 from brownie import FujiCollectible
-from
+from scripts.helpful_scripts import fund_advanced_collectible
+
+def main():
+    fuji_collectible = FujiCollectible[len(FujiCollectible) - 1]
+    fund_advanced_collectible[fuji_collectible]
